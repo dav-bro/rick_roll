210f4e4e3ff17797bae1124c1ab07b3f110c2087
David Brodbeck <david@joinpult.com>
2022-12-27 20:36:59 +1100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import IMG from "./img.png"
 
-
+const THRESH_SEC = 10;
 const App = () => {
 
-
+    const [cancel, setCancel] = useState(false);
+    const [seconds, setSeconds] = useState(0);
 
      useEffect(() => {
-          setTimeout(() => window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 10000)
-     }, [])
+         if (!cancel) {
+             setTimeout(() => setSeconds(s => s + 1), 1);
+             if (seconds >= THRESH_SEC * 1000) {
+                 window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+             }
+         }
+         else {
+             setSeconds(0);
+         }
 
+     }, [cancel, seconds])
+
+    console.log("CANCEL", cancel, seconds/1000)
   return (
-    <div className={"full middle"}>
+    <div className={"full middle"} onClick={() => setCancel(c => !c)}>
         <img src={IMG}/>
     </div>
   );
